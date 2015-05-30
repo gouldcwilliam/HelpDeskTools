6082c0c2f6c19a7dee6409d8a649f1fc7f3705a5
Chad Gould <gouldcwilliam@gmail.com>
2015-05-30 10:29:09 -0400

    Hammering out issues with the COM assembly for microsoft.interop.excel

--- HelpDeskTools/Libraries/HDSharedServices/Properties/AssemblyInfo.cs
+++ HelpDeskTools/Libraries/HDSharedServices/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d3f6d017-c4e3-4ca3-9dac-add8b8b49903")]
