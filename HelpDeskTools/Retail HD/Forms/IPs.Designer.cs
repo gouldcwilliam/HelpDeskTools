8c86d5117e0012f5c364be348bd3b8da50fef261
gouldcwilliam <gouldcwilliam@gmail.com>
2015-12-27 14:53:37 -0500

    added vlan ip in addition to the vlan subnet

--- HelpDeskTools/Retail HD/Forms/IPs.Designer.cs
+++ HelpDeskTools/Retail HD/Forms/IPs.Designer.cs
@@ -40,12 +40,30 @@
 			this.v50 = new System.Windows.Forms.TextBox();
 			this.groupBox5 = new System.Windows.Forms.GroupBox();
 			this.v60 = new System.Windows.Forms.TextBox();
+			this.groupBox6 = new System.Windows.Forms.GroupBox();
+			this.i10 = new System.Windows.Forms.TextBox();
+			this.groupBox7 = new System.Windows.Forms.GroupBox();
+			this.i20 = new System.Windows.Forms.TextBox();
+			this.groupBox8 = new System.Windows.Forms.GroupBox();
+			this.i30 = new System.Windows.Forms.TextBox();
+			this.groupBox9 = new System.Windows.Forms.GroupBox();
+			this.i40 = new System.Windows.Forms.TextBox();
+			this.groupBox10 = new System.Windows.Forms.GroupBox();
+			this.i50 = new System.Windows.Forms.TextBox();
+			this.groupBox11 = new System.Windows.Forms.GroupBox();
+			this.i60 = new System.Windows.Forms.TextBox();
 			this.gbAddress.SuspendLayout();
 			this.groupBox1.SuspendLayout();
 			this.groupBox2.SuspendLayout();
 			this.groupBox3.SuspendLayout();
 			this.groupBox4.SuspendLayout();
 			this.groupBox5.SuspendLayout();
+			this.groupBox6.SuspendLayout();
+			this.groupBox7.SuspendLayout();
+			this.groupBox8.SuspendLayout();
+			this.groupBox9.SuspendLayout();
+			this.groupBox10.SuspendLayout();
+			this.groupBox11.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// gbAddress
@@ -54,7 +72,7 @@
 			this.gbAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.gbAddress.Location = new System.Drawing.Point(12, 12);
 			this.gbAddress.Name = "gbAddress";
-			this.gbAddress.Size = new System.Drawing.Size(208, 30);
+			this.gbAddress.Size = new System.Drawing.Size(162, 30);
 			this.gbAddress.TabIndex = 55;
 			this.gbAddress.TabStop = false;
 			this.gbAddress.Text = "VLAN 10";
@@ -67,10 +85,11 @@
 			this.v10.Location = new System.Drawing.Point(6, 14);
 			this.v10.Name = "v10";
 			this.v10.ReadOnly = true;
-			this.v10.Size = new System.Drawing.Size(196, 13);
+			this.v10.Size = new System.Drawing.Size(150, 13);
 			this.v10.TabIndex = 0;
 			this.v10.TabStop = false;
 			this.v10.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			this.v10.Click += new System.EventHandler(this.txt_Click);
 			// 
 			// groupBox1
 			// 
@@ -78,7 +97,7 @@
 			this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.groupBox1.Location = new System.Drawing.Point(12, 45);
 			this.groupBox1.Name = "groupBox1";
-			this.groupBox1.Size = new System.Drawing.Size(208, 30);
+			this.groupBox1.Size = new System.Drawing.Size(162, 30);
 			this.groupBox1.TabIndex = 55;
 			this.groupBox1.TabStop = false;
 			this.groupBox1.Text = "VLAN 20";
@@ -91,10 +110,11 @@
 			this.v20.Location = new System.Drawing.Point(6, 14);
 			this.v20.Name = "v20";
 			this.v20.ReadOnly = true;
-			this.v20.Size = new System.Drawing.Size(196, 13);
+			this.v20.Size = new System.Drawing.Size(150, 13);
 			this.v20.TabIndex = 0;
 			this.v20.TabStop = false;
 			this.v20.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			this.v20.Click += new System.EventHandler(this.txt_Click);
 			// 
 			// groupBox2
 			// 
@@ -102,7 +122,7 @@
 			this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.groupBox2.Location = new System.Drawing.Point(12, 81);
 			this.groupBox2.Name = "groupBox2";
-			this.groupBox2.Size = new System.Drawing.Size(208, 30);
+			this.groupBox2.Size = new System.Drawing.Size(162, 30);
 			this.groupBox2.TabIndex = 55;
 			this.groupBox2.TabStop = false;
 			this.groupBox2.Text = "VLAN 30";
@@ -115,10 +135,11 @@
 			this.v30.Location = new System.Drawing.Point(6, 14);
 			this.v30.Name = "v30";
 			this.v30.ReadOnly = true;
-			this.v30.Size = new System.Drawing.Size(196, 13);
+			this.v30.Size = new System.Drawing.Size(150, 13);
 			this.v30.TabIndex = 0;
 			this.v30.TabStop = false;
 			this.v30.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			this.v30.Click += new System.EventHandler(this.txt_Click);
 			// 
 			// groupBox3
 			// 
@@ -126,7 +147,7 @@
 			this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.groupBox3.Location = new System.Drawing.Point(12, 117);
 			this.groupBox3.Name = "groupBox3";
-			this.groupBox3.Size = new System.Drawing.Size(208, 30);
+			this.groupBox3.Size = new System.Drawing.Size(162, 30);
 			this.groupBox3.TabIndex = 55;
 			this.groupBox3.TabStop = false;
 			this.groupBox3.Text = "VLAN 40";
@@ -139,10 +160,11 @@
 			this.v40.Location = new System.Drawing.Point(6, 14);
 			this.v40.Name = "v40";
 			this.v40.ReadOnly = true;
-			this.v40.Size = new System.Drawing.Size(196, 13);
+			this.v40.Size = new System.Drawing.Size(150, 13);
 			this.v40.TabIndex = 0;
 			this.v40.TabStop = false;
 			this.v40.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			this.v40.Click += new System.EventHandler(this.txt_Click);
 			// 
 			// groupBox4
 			// 
@@ -150,7 +172,7 @@
 			this.groupBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.groupBox4.Location = new System.Drawing.Point(12, 153);
 			this.groupBox4.Name = "groupBox4";
-			this.groupBox4.Size = new System.Drawing.Size(208, 30);
+			this.groupBox4.Size = new System.Drawing.Size(162, 30);
 			this.groupBox4.TabIndex = 55;
 			this.groupBox4.TabStop = false;
 			this.groupBox4.Text = "VLAN 50";
@@ -163,10 +185,11 @@
 			this.v50.Location = new System.Drawing.Point(6, 14);
 			this.v50.Name = "v50";
 			this.v50.ReadOnly = true;
-			this.v50.Size = new System.Drawing.Size(196, 13);
+			this.v50.Size = new System.Drawing.Size(150, 13);
 			this.v50.TabIndex = 0;
 			this.v50.TabStop = false;
 			this.v50.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			this.v50.Click += new System.EventHandler(this.txt_Click);
 			// 
 			// groupBox5
 			// 
@@ -174,7 +197,7 @@
 			this.groupBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.groupBox5.Location = new System.Drawing.Point(12, 189);
 			this.groupBox5.Name = "groupBox5";
-			this.groupBox5.Size = new System.Drawing.Size(208, 30);
+			this.groupBox5.Size = new System.Drawing.Size(162, 30);
 			this.groupBox5.TabIndex = 55;
 			this.groupBox5.TabStop = false;
 			this.groupBox5.Text = "VLAN 60";
@@ -187,16 +210,173 @@
 			this.v60.Location = new System.Drawing.Point(6, 14);
 			this.v60.Name = "v60";
 			this.v60.ReadOnly = true;
-			this.v60.Size = new System.Drawing.Size(196, 13);
+			this.v60.Size = new System.Drawing.Size(150, 13);
 			this.v60.TabIndex = 0;
 			this.v60.TabStop = false;
 			this.v60.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			this.v60.Click += new System.EventHandler(this.txt_Click);
+			// 
+			// groupBox6
+			// 
+			this.groupBox6.Controls.Add(this.i10);
+			this.groupBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.groupBox6.Location = new System.Drawing.Point(174, 12);
+			this.groupBox6.Name = "groupBox6";
+			this.groupBox6.Size = new System.Drawing.Size(162, 30);
+			this.groupBox6.TabIndex = 56;
+			this.groupBox6.TabStop = false;
+			this.groupBox6.Text = "VLAN 10";
+			// 
+			// i10
+			// 
+			this.i10.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.i10.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.i10.Location = new System.Drawing.Point(6, 14);
+			this.i10.Name = "i10";
+			this.i10.ReadOnly = true;
+			this.i10.Size = new System.Drawing.Size(150, 13);
+			this.i10.TabIndex = 0;
+			this.i10.TabStop = false;
+			this.i10.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			this.i10.Click += new System.EventHandler(this.txt_Click);
+			// 
+			// groupBox7
+			// 
+			this.groupBox7.Controls.Add(this.i20);
+			this.groupBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.groupBox7.Location = new System.Drawing.Point(174, 45);
+			this.groupBox7.Name = "groupBox7";
+			this.groupBox7.Size = new System.Drawing.Size(162, 30);
+			this.groupBox7.TabIndex = 56;
+			this.groupBox7.TabStop = false;
+			this.groupBox7.Text = "VLAN 20";
+			// 
+			// i20
+			// 
+			this.i20.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.i20.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.i20.Location = new System.Drawing.Point(6, 14);
+			this.i20.Name = "i20";
+			this.i20.ReadOnly = true;
+			this.i20.Size = new System.Drawing.Size(150, 13);
+			this.i20.TabIndex = 0;
+			this.i20.TabStop = false;
+			this.i20.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			this.i20.Click += new System.EventHandler(this.txt_Click);
+			// 
+			// groupBox8
+			// 
+			this.groupBox8.Controls.Add(this.i30);
+			this.groupBox8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.groupBox8.Location = new System.Drawing.Point(174, 81);
+			this.groupBox8.Name = "groupBox8";
+			this.groupBox8.Size = new System.Drawing.Size(162, 30);
+			this.groupBox8.TabIndex = 56;
+			this.groupBox8.TabStop = false;
+			this.groupBox8.Text = "VLAN 30";
+			// 
+			// i30
+			// 
+			this.i30.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.i30.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.i30.Location = new System.Drawing.Point(6, 14);
+			this.i30.Name = "i30";
+			this.i30.ReadOnly = true;
+			this.i30.Size = new System.Drawing.Size(150, 13);
+			this.i30.TabIndex = 0;
+			this.i30.TabStop = false;
+			this.i30.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			this.i30.Click += new System.EventHandler(this.txt_Click);
+			// 
+			// groupBox9
+			// 
+			this.groupBox9.Controls.Add(this.i40);
+			this.groupBox9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.groupBox9.Location = new System.Drawing.Point(174, 117);
+			this.groupBox9.Name = "groupBox9";
+			this.groupBox9.Size = new System.Drawing.Size(162, 30);
+			this.groupBox9.TabIndex = 56;
+			this.groupBox9.TabStop = false;
+			this.groupBox9.Text = "VLAN 40";
+			// 
+			// i40
+			// 
+			this.i40.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.i40.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.i40.Location = new System.Drawing.Point(6, 14);
+			this.i40.Name = "i40";
+			this.i40.ReadOnly = true;
+			this.i40.Size = new System.Drawing.Size(150, 13);
+			this.i40.TabIndex = 0;
+			this.i40.TabStop = false;
+			this.i40.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			this.i40.Click += new System.EventHandler(this.txt_Click);
+			// 
+			// groupBox10
+			// 
+			this.groupBox10.Controls.Add(this.i50);
+			this.groupBox10.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.groupBox10.Location = new System.Drawing.Point(174, 153);
+			this.groupBox10.Name = "groupBox10";
+			this.groupBox10.Size = new System.Drawing.Size(162, 30);
+			this.groupBox10.TabIndex = 56;
+			this.groupBox10.TabStop = false;
+			this.groupBox10.Text = "VLAN 50";
+			// 
+			// i50
+			// 
+			this.i50.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.i50.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.i50.Location = new System.Drawing.Point(6, 14);
+			this.i50.Name = "i50";
+			this.i50.ReadOnly = true;
+			this.i50.Size = new System.Drawing.Size(150, 13);
+			this.i50.TabIndex = 0;
+			this.i50.TabStop = false;
+			this.i50.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			this.i50.Click += new System.EventHandler(this.txt_Click);
+			// 
+			// groupBox11
+			// 
+			this.groupBox11.Controls.Add(this.i60);
+			this.groupBox11.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.groupBox11.Location = new System.Drawing.Point(174, 189);
+			this.groupBox11.Name = "groupBox11";
+			this.groupBox11.Size = new System.Drawing.Size(162, 30);
+			this.groupBox11.TabIndex = 56;
+			this.groupBox11.TabStop = false;
+			this.groupBox11.Text = "VLAN 60";
+			// 
+			// i60
+			// 
+			this.i60.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.i60.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.i60.Location = new System.Drawing.Point(6, 14);
+			this.i60.Name = "i60";
+			this.i60.ReadOnly = true;
+			this.i60.Size = new System.Drawing.Size(150, 13);
+			this.i60.TabIndex = 0;
+			this.i60.TabStop = false;
+			this.i60.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			this.i60.Click += new System.EventHandler(this.txt_Click);
 			// 
 			// IPs
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(231, 239);
+			this.ClientSize = new System.Drawing.Size(355, 239);
+			this.Controls.Add(this.groupBox11);
+			this.Controls.Add(this.groupBox10);
+			this.Controls.Add(this.groupBox9);
+			this.Controls.Add(this.groupBox8);
+			this.Controls.Add(this.groupBox7);
+			this.Controls.Add(this.groupBox6);
 			this.Controls.Add(this.groupBox5);
 			this.Controls.Add(this.groupBox4);
 			this.Controls.Add(this.groupBox3);
@@ -219,6 +399,18 @@
 			this.groupBox4.PerformLayout();
 			this.groupBox5.ResumeLayout(false);
 			this.groupBox5.PerformLayout();
+			this.groupBox6.ResumeLayout(false);
+			this.groupBox6.PerformLayout();
+			this.groupBox7.ResumeLayout(false);
+			this.groupBox7.PerformLayout();
+			this.groupBox8.ResumeLayout(false);
+			this.groupBox8.PerformLayout();
+			this.groupBox9.ResumeLayout(false);
+			this.groupBox9.PerformLayout();
+			this.groupBox10.ResumeLayout(false);
+			this.groupBox10.PerformLayout();
+			this.groupBox11.ResumeLayout(false);
+			this.groupBox11.PerformLayout();
 			this.ResumeLayout(false);
 
 		}
@@ -237,5 +429,17 @@
 		public System.Windows.Forms.TextBox v50;
 		public System.Windows.Forms.GroupBox groupBox5;
 		public System.Windows.Forms.TextBox v60;
+		public System.Windows.Forms.GroupBox groupBox6;
+		public System.Windows.Forms.TextBox i10;
+		public System.Windows.Forms.GroupBox groupBox7;
+		public System.Windows.Forms.TextBox i20;
+		public System.Windows.Forms.GroupBox groupBox8;
+		public System.Windows.Forms.TextBox i30;
+		public System.Windows.Forms.GroupBox groupBox9;
+		public System.Windows.Forms.TextBox i40;
+		public System.Windows.Forms.GroupBox groupBox10;
+		public System.Windows.Forms.TextBox i50;
+		public System.Windows.Forms.GroupBox groupBox11;
+		public System.Windows.Forms.TextBox i60;
 	}
 }
--- HelpDeskTools/Retail HD/Forms/IPs.cs
+++ HelpDeskTools/Retail HD/Forms/IPs.cs
@@ -24,12 +24,34 @@ namespace Retail_HD.Forms
 			foreach (System.Data.DataRow dr in dt.Rows)
 			{
 				v10.Text = dr["VLAN 10"].ToString();
+				i10.Text = getIP(v10.Text);
 				v20.Text = dr["VLAN 20"].ToString();
+				i20.Text = getIP(v20.Text);
 				v30.Text = dr["VLAN 30"].ToString();
+				i30.Text = getIP(v30.Text);
 				v40.Text = dr["VLAN 40"].ToString();
+				i40.Text = getIP(v40.Text);
 				v50.Text = dr["VLAN 50"].ToString();
+				i50.Text = getIP(v50.Text);
 				v60.Text = dr["VLAN 60"].ToString();
+				i60.Text = getIP(v60.Text);
 			}
+		}
+
+		private void txt_Click(object sender, EventArgs e)
+		{
+			TextBox tb = (TextBox)sender;
+			tb.SelectAll();
+		}
+		private string getIP(string vlan)
+		{
+			if (vlan == "") { return ""; }
+			string[] va = vlan.Split('.');
+			va[3] = va[3].Replace("/25", "");
+			int i = 0;
+            int.TryParse(va[3], out i);
+			va[3] = (i + 1).ToString();
+			return va[0] + "." + va[1] + "." + va[2] + "." + va[3];
 		}
 	}
 }
