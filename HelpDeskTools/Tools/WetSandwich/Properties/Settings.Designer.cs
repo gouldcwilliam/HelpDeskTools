8a0e085859afae1280cf3bcbb2b5fac4c3a1374e
gouldcwilliam <gouldcwilliam@gmail.com>
2016-05-23 16:53:42 -0400

    reverted - needs list action fast forward and the annette form

--- HelpDeskTools/Tools/WetSandwich/Properties/Settings.Designer.cs
+++ HelpDeskTools/Tools/WetSandwich/Properties/Settings.Designer.cs
@@ -103,6 +103,18 @@ namespace WetSandwich.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("4.2.12.139")]
+        public string multiVersion {
+            get {
+                return ((string)(this["multiVersion"]));
+            }
+            set {
+                this["multiVersion"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("2.0.0.926")]
         public string redIronVersion {
             get {
@@ -115,6 +127,18 @@ namespace WetSandwich.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("4.3.7-20150824")]
+        public string vfVersion {
+            get {
+                return ((string)(this["vfVersion"]));
+            }
+            set {
+                this["vfVersion"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("402,403,404,545,546")]
         public string ignore {
             get {
@@ -122,35 +146,6 @@ namespace WetSandwich.Properties {
             }
             set {
                 this["ignore"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsi=\"http://www.w3." +
-            "org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  <s" +
-            "tring>4.2.12.139</string>\r\n  <string>4.2.12.161</string>\r\n</ArrayOfString>")]
-        public global::System.Collections.Specialized.StringCollection _multiVersions {
-            get {
-                return ((global::System.Collections.Specialized.StringCollection)(this["_multiVersions"]));
-            }
-            set {
-                this["_multiVersions"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsi=\"http://www.w3." +
-            "org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  <s" +
-            "tring>4.4.5-20151127</string>\r\n  <string>4.3.7-20150824</string>\r\n</ArrayOfStrin" +
-            "g>")]
-        public global::System.Collections.Specialized.StringCollection _vfVersions {
-            get {
-                return ((global::System.Collections.Specialized.StringCollection)(this["_vfVersions"]));
-            }
-            set {
-                this["_vfVersions"] = value;
             }
         }
     }
